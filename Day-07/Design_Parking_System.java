a45a88ffe07b470816a6adb2d63aa726526813ed
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-09-07 22:47:34 +0530

    leetcode: add  Design Parking System solution

---
+++ Day-07/Design_Parking_System.java
@@ -0,0 +1,14 @@
+class ParkingSystem {
+      public int [ ] slots;
+    public ParkingSystem(int big, int medium, int small) {
+        slots = new int []{big,medium,small};
+    }
+    
+    public boolean addCar(int carType) {
+        if(slots[carType-1]>0){
+            slots[carType-1]--;
+            return true;
+        }return false;
+    }
+}
+
